72cc17881b7fbb09ea76cc813904b04e5f4a6604
makoo0837 <towawaa74@gmail.com>
2026-09-11 23:21:56 -0700

    Agregar vista de monitoreo y sidebar

---
+++ ProyectoWong/Controllers/MonitoreoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoWong.Controllers
+{
+    public class MonitoreoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Produccion()
+        {
+            return View("MonitoreoProduccion");
+        }
+    }
+}
---
+++ ProyectoWong/Controllers/MonitoreoProduccion.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoWong.Controllers
+{
+    public class MonitoreoProduccion : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
